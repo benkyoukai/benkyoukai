a130ad3c7ebe1da384d3eabf2c9c714310e178c3
wenjun.yan <mylastnameisyan@gmail.com>
2014-10-07 01:11:21 +0900

    add merge_sort.py

---
+++ algorithm/merge_sort/merge_sort.py
@@ -0,0 +1,42 @@
+def merge_sort(nums, cmp=cmp):
+    length = len(nums)
+    if length < 2:
+        return nums
+
+    left_length = length / 2
+    right_length = length - length / 2
+    left = merge_sort(nums[0: left_length])
+    right = merge_sort(nums[left_length:])
+    merged = []
+
+    i, j = 0, 0
+    while True:
+        if i == left_length:
+            merged.extend(right[j:])
+            break
+
+        if j == right_length:
+            merged.extend(left[i:])
+            break
+
+        if cmp(left[i], right[j]) < 0:
+            merged.append(left[i])
+            i += 1
+        else:
+            merged.append(right[j])
+            j += 1
+
+    return merged
+
+# test
+import random
+
+for i in range(10):
+    size = random.randrange(100)
+    t1 = random.sample(xrange(100), size)
+    assert merge_sort(t1) == sorted(t1)
+
+assert merge_sort([]) == []
+assert merge_sort([1]) == [1]
+
+print "=> passed"
